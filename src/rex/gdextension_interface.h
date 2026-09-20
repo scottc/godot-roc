691821bba0aa9cfec939b4269e3c91990b8355f3
scottc <scottc@users.noreply.github.com>
2026-09-20 20:08:38 +0800

    Add bindings files for multiple engines

---
+++ src/rex/gdextension_interface.h
@@ -0,0 +1,5 @@
+Dumping GDExtension interface header file
+ReX Engine v0.0.1.alpha.898. - https://redotengine.org
+
+ERROR: Cannot open file 'gdextension_interface.h' for writing.
+   at: generate_gdextension_interface_file (/builds/redot-engine/rex-engine/bin/core/extension/gdextension_interface_dump.gen.h:780)
